c6a046051eb1fdaf497a2ee5f68f9d531033b298
viradiya_om <viradiyaom003@gmail.com>
2025-06-29 23:40:51 +0530

    updated connection request

--- index.js
+++ index.js
@@ -70,6 +70,10 @@ const fetchStateFromFile = () => {
 io.on("connection", (socket) => {
   logger("✅ A user connected");
 
+  socket.on("connect-server", () => {
+    socket.emit("connected-server");
+  });
+
   socket.on("join-room", (roomId) => {
     if (!rooms[roomId]) {
       logger(`🚨 Room not found ${roomId}`);
@@ -109,42 +113,66 @@ io.on("connection", (socket) => {
     }
   });
 
-  socket.on("create-room", (roomId) => {
-    // if (rooms[roomId].ownerId) {
-    //   logger(`🚨 Host already active for ${roomId}`);
-    //   socket.emit("join-room", {
-    //     type: "ERROR",
-    //     message: "Host already active for this room",
-    //   });
-    //   return;
-    // }
-    socket.join(roomId);
+  socket.on(
+    "create-room",
+    ({
+      roomId,
+      allowMemberToPlay,
+      allowMemberToSync,
+      allowMemberControlVolume,
+    }) => {
+      // if (rooms[roomId].ownerId) {
+      //   logger(`🚨 Host already active for ${roomId}`);
+      //   socket.emit("join-room", {
+      //     type: "ERROR",
+      //     message: "Host already active for this room",
+      //   });
+      //   return;
+      // }
+      socket.join(roomId);
 
-    if (!rooms[roomId]) {
-      logger(`🔗 Created new room: ${roomId}`);
-      rooms[roomId] = {
-        roomId: roomId,
-        createdAt: new Date().toISOString(),
-        currentPlaying: 0,
-        isPlaying: false,
-        allowMemberToPlay: true,
-        alsoPlayInMember: false,
-        ownerId: socket.id,
-        tracks: [],
-      };
-    } else {
-      rooms[roomId].ownerId = socket.id;
+      if (!rooms[roomId]) {
+        logger(`🔗 Created new room: ${roomId}`);
+        rooms[roomId] = {
+          roomId: roomId,
+          createdAt: new Date().toISOString(),
+          currentPlaying: -1,
+          volume: 100,
+          isPlaying: false,
+          allowMemberToPlay,
+          allowMemberToSync,
+          allowMemberControlVolume,
+          activeMembers: [socket.id],
+          alsoPlayInMember: false,
+          ownerId: socket.id,
+          tracks: [],
+        };
+        console.log("🚀 - io.on - rooms:", rooms[roomId]);
+      } else {
+        rooms[roomId] = {
+          ...rooms[roomId],
+          currentPlaying: -1,
+          volume: 100,
+          isPlaying: false,
+          allowMemberToPlay,
+          allowMemberToSync,
+          allowMemberControlVolume,
+          activeMembers: [socket.id],
+          alsoPlayInMember: false,
+          ownerId: socket.id,
+        };
+      }
+      owners[socket.id] = roomId;
+      members[socket.id] = roomId;
+      saveRoomState(roomId);
+      const { tracks, ...rest } = rooms[roomId];
+      socket.emit("room-tracks", tracks);
+      socket.emit("join-room", {
+        type: "SUCCESS",
+        ...rest,
+      });
     }
-    owners[socket.id] = roomId;
-    members[socket.id] = roomId;
-    saveRoomState(roomId);
-    const { tracks, ...rest } = rooms[roomId];
-    socket.emit("room-tracks", tracks);
-    socket.emit("join-room", {
-      type: "SUCCESS",
-      ...rest,
-    });
-  });
+  );
 
   socket.on("add-track", ({ tracks }) => {
     const roomId = members[socket.id];
@@ -170,6 +198,7 @@ io.on("connection", (socket) => {
 
   socket.on("update-current-playing", ({ index }) => {
     const roomId = members[socket.id];
+    console.log("🚀 - socket.on - index:", index, roomId);
     if (!rooms[roomId]) {
       logger(`🚨 Room not found ${roomId}`);
       return;
@@ -209,10 +238,28 @@ io.on("connection", (socket) => {
     io.to(roomId).emit("sync-response", data);
   });
 
+  socket.on("update-volume", (value) => {
+    const roomId = members[socket.id];
+    if (!rooms[roomId]) {
+      logger(`🚨 Room not found ${roomId}`);
+      return;
+    }
+    if (rooms[roomId].volume === value) return;
+    rooms[roomId].volume = value;
+    io.to(roomId).emit("update-volume", value);
+  });
+
+  socket.on("client-active", () => {
+    console.log("Client is active (to avoid render shutdown)");
+  });
+
   socket.on("disconnect", () => {
     logger("❌ A user disconnected");
     if (owners[socket.id] && rooms[owners[socket.id]]) {
       rooms[owners[socket.id]].ownerId = "";
+      rooms[owners[socket.id]].volume = 100;
+      rooms[owners[socket.id]].currentPlaying = 0;
+      rooms[owners[socket.id]].activeMembers = [];
       saveRoomState(owners[socket.id]);
       io.to(owners[socket.id]).emit("clear-state");
       delete owners[socket.id];
